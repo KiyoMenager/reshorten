d06baadbd990bbcc325cee381912cc22e041936d
Kiyo <menager.kiyoaki@gmail.com>
2018-10-26 19:28:05 +0200

    Add WEB API layer for links

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope path: 'api' do
+    resources :links, only: [:show, :create, :update, :destroy], param: :short_code
+  end
 end
