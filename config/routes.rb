65d3d5b3723cf10deacbef89322b9cb789dc2977
Kiyo <menager.kiyoaki@gmail.com>
2018-10-31 14:06:48 +0100

    Add WEB API layer for shortcode->url redirections

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/:short_code', to: 'redirections#show'
+
   scope path: 'api' do
     resources :links, only: [:show, :create, :update, :destroy], param: :short_code
   end
